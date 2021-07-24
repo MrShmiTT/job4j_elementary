355b8f1b9870abb1c4f52be8b8f06806c7b49cec
Alex <alexandr.vatutin@gmail.com>
2021-07-24 15:16:24 +0300

    StudentInfo

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksandr Vatutin");
+        System.out.println("09.06.1987");
+    }
+}
