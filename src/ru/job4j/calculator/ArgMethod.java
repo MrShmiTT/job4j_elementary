b747107070a3358aed8bfb1ee6bbac5c68bfcf2b
Alex <alexandr.vatutin@gmail.com>
2021-07-24 15:21:43 +0300

    StudentInfo

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+        public static void hello (String hi, String name) {
+            System.out.println(hi + name);
+        }
+
+    public static void main(String[] args) {
+        String hi = "Hello, ";
+        String name = "Job4j";
+        ArgMethod.hello(hi, name);
+        ArgMethod.hello(hi, name);
+        ArgMethod.hello(hi, name);
+        ArgMethod.hello(hi, name);
+    }
+}
