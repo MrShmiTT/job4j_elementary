1b4131a4a8c1b72a9e949e9c37b59be3a90b0748
Alex <alexandr.vatutin@gmail.com>
2021-07-27 17:34:56 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -1,14 +1,9 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-        new Fact().calc(-1);
-
-    }
-
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n should be > 0");
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
@@ -17,3 +12,23 @@ public class Fact {
         return rsl;
     }
 }
+
+//package ru.job4j.ex;
+//
+//public class Fact {
+//    public static void main(String[] args) {
+//        new Fact().calc(-1);
+//
+//    }
+//
+//    public int calc(int n) {
+//        if (n < 0) {
+//            throw new IllegalArgumentException("n should be > 0");
+//        }
+//        int rsl = 1;
+//        for (int index = 1; index <= n; index++) {
+//            rsl *= index;
+//        }
+//        return rsl;
+//    }
+//}
