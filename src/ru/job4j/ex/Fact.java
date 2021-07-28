fc7b63091a5af21c947c2f25ccbaf50da117b2f8
Alex <alexandr.vatutin@gmail.com>
2021-07-28 13:44:44 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #182366]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -16,19 +16,3 @@ public class Fact {
         return rsl;
     }
 }
-
-
-//package ru.job4j.ex;
-//
-//public class Fact {
-//    public static int calc(int n) {
-//        if (n < 0) {
-//            throw new IllegalArgumentException("N could not be less then 0");
-//        }
-//        int rsl = 1;
-//        for (int index = 1; index <= n; index++) {
-//            rsl *= index;
-//        }
-//        return rsl;
-//    }
-//}
