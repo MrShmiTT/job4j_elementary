a452b06fabaaf78cfd5c36da274f332498d37a18
Alex <alexandr.vatutin@gmail.com>
2021-08-06 22:10:57 +0300

    1. Чтение кода и базовые ошибки [#40205]

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < rsl.length; k++) {
+            if (i > left.length - 1) {
+                int temp = right[j];
+                rsl[k] = temp;
+                j++;
+            } else if (j > right.length - 1) {
+                int temp = left[i];
+                rsl[k] = temp;
+                i++;
+            } else if (left[i] < right[j]) {
+                int temp = left[i];
+                rsl[k] = temp;
+                i++;
+            } else {
+                int temp = right[j];
+                rsl[k] = temp;
+                j++;
+            }
+        }
+        return rsl;
+    }
+}
