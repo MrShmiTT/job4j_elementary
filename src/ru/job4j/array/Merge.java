3ce50aec641b6cf595e915a2901fcfca482b8a08
Alex <alexandr.vatutin@gmail.com>
2021-08-06 22:16:50 +0300

    Слияние двух массивов с комментариями

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,25 +1,26 @@
 package ru.job4j.array;
+/*  Даны два отсортированных по возрастанию массива. Как без сортировки их объединить в третий массив? */
 
 public class Merge {
-    public static int[] merge(int[] left, int[] right) {
-        int[] rsl = new int[left.length + right.length];
-        int i = 0;
-        int j = 0;
+    public static int[] merge(int[] left, int[] right) {            // массивы, которые нужно слить
+        int[] rsl = new int[left.length + right.length];            // результирующий массив и вычисление его длины
+        int i = 0;                                                  //  индексы для массивов left и right
+        int j = 0;                                                  // которые указывают на текущие элементы на каждом шаге и образуют буфер для перемещения.
         for (int k = 0; k < rsl.length; k++) {
-            if (i > left.length - 1) {
+            if (i > left.length - 1) {                              // проверяет, что бы индексы не вышли за переделы количества элементов в массивах.
                 int temp = right[j];
                 rsl[k] = temp;
                 j++;
-            } else if (j > right.length - 1) {
+            } else if (j > right.length - 1) {                      // проверяет, что бы индексы не вышли за переделы количества элементов в массивах.
                 int temp = left[i];
                 rsl[k] = temp;
                 i++;
-            } else if (left[i] < right[j]) {
+            } else if (left[i] < right[j]) {                        // обеспечивает перемещение в массив наименьшего элемента из первого массива
                 int temp = left[i];
                 rsl[k] = temp;
                 i++;
             } else {
-                int temp = right[j];
+                int temp = right[j];                                // обеспечивает перемещение в массив наименьшего элемента из второго массива
                 rsl[k] = temp;
                 j++;
             }
