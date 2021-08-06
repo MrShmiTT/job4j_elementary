30db157b7605c46e3cf9432a4c4d01e1c07c7203
Alex <alexandr.vatutin@gmail.com>
2021-08-06 10:45:23 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #186375]

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-        int[] rsl = new int[100];                                       // массив, содержащий монеты, которыми дали сдачу
-        int size = 0;                                                   // количество монет, т.е. размер массива rsl
-        int sdacha = money - price;                                     // размер нашей сдачи за кофе
-        for (int i = 0; i < coins.length; i++) {                        // начинаем перебор ячеек с монетами
-            while (sdacha - coins[i] >= 0) {                            // наичнаем перебор сдачи которую нам еще должен выдать автомат
-                sdacha = sdacha - coins[i];                             // счиатем размер оставшейся сдачи
-                rsl[size] = coins[i];                                   // количество монет равно количеству раз, сколько выпали монеты
+        int[] rsl = new int[100];
+        int size = 0;
+        int sdacha = money - price;
+        for (int coin : coins) {
+            while (sdacha - coin >= 0) {
+                sdacha = sdacha - coin;
+                rsl[size] = coin;
                 size++;
             }
         }
